f5f8dea94b8a6ed4d39b70c24a6c99072a1941fb
Polk <polkpdpddp@gmail.com>
2026-09-15 17:47:29 +0300

    fixxxx

--- ldpc_py/decoder_factory.py
+++ ldpc_py/decoder_factory.py
@@ -1,7 +1,6 @@
 from .bin_ldpc_bf import BinLdpcBfDecoder
 from .bin_ldpc_ms import BinLdpcMsDecoder
 from .bin_ldpc_mgdbf import BinLdpcMgdbfDecoder
-from .bin_ldpc_sigmoid import BinLdpcSigmoidDecoder
 from .bin_ldpc_sigmoid1 import BinLdpcSigmoid1Decoder
 from .bin_ldpc_pmgdbf import BinLdpcPmgdbfDecoder
 from .bin_ldpc_epmgdbf import BinLdpcEpmgdbfDecoder
@@ -15,7 +14,6 @@ _DECODER_TYPES = {
     "bit-flipping": BinLdpcBfDecoder,
     "min-sum": BinLdpcMsDecoder,
     "multi gradient descent bit-flipping": BinLdpcMgdbfDecoder,
-    "sigmoid multi gradient descent bit-flipping": BinLdpcSigmoidDecoder,
     "sigmoid1 probabilistic momentum gradient descent bit-flipping": BinLdpcSigmoid1Decoder,
     "probabilistic momentum gradient descent bit-flipping": BinLdpcPmgdbfDecoder,
     "erasure probabilistic momentum gradient descent bit-flipping": BinLdpcEpmgdbfDecoder,
