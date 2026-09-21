fa72cef3303fbe107aab75e6cf8b05db2600fc27
Polk <polkpdpddp@gmail.com>
2026-09-21 16:03:20 +0300

    fix

--- tune_cpp_mgdms.py
+++ tune_cpp_mgdms.py
@@ -18,11 +18,16 @@ PROJECT_DIR = Path(__file__).resolve().parent
 DEFAULT_CONFIG = PROJECT_DIR / "experiments" / "experiment_cpp_mgdms.json"
 DEFAULT_OUTPUT = PROJECT_DIR / "params_cpp_mgdms.txt"
 
-DEFAULT_LEARNING_RATES = tuple(np.round(np.arange(0.04, 1.0 + 1e-9, 0.04), 6))
+import numpy as np
+
+
+DEFAULT_LEARNING_RATES = tuple(np.round(np.arange(0.2, 1.0 + 1e-9, 0.2), 6))
 DEFAULT_LEARNING_RATE_DECAYS = (0.0, 0.01, 0.03, 0.05, 0.1)
 DEFAULT_ALPHAS = tuple(np.round(np.arange(0.25, 3.0 + 1e-9, 0.25), 6))
-DEFAULT_L2 = (0.0, 0.5, 1.0, 1.2)
-DEFAULT_MOMENTUM_VALUES = tuple(np.round(np.arange(0.0, 0.95 + 1e-9, 0.05), 6))
+DEFAULT_L2 = (0.0,0.6, 1.0, 1.2)
+DEFAULT_MOMENTUM_VALUES = tuple(np.round(np.arange(0.0, 0.96 + 1e-9, 0.04), 6))
+
+
 
 _BASE_EXPERIMENT = None
 _SNR_DB = None
